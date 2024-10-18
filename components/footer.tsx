4842be092646dfe8c35ddc1eee63c17a9c212402
rk-rishikesh <rvk.rishikeshkale@gmail.com>
2024-10-18 18:24:47 +0530

    Updated Discord Link

--- components/footer.tsx
+++ components/footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
 
         <a
           className="text-black items-center inline-flex bg-white border-2 border-black duration-200 ease-in-out focus:outline-none hover:bg-black hover:shadow-none hover:text-white justify-center rounded-full shadow-[5px_5px_black] text-center transform transition w-full px-2 py-2"
-          href="https://discord.gg/QrRQ2fTs"
+          href="https://discord.gg/filecoin"
         >
           <Image
             src="/assets/logos/discord-logo.png"
